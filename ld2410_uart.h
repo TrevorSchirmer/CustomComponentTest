5b953b2c213260561f2aaebb9b7578325c150337
Trevor Schirmer <trevor.schirmer@gmail.com>
2023-07-24 14:38:53 -0400

    mmWave Working

--- ld2410_uart.h
+++ ld2410_uart.h
@@ -1,67 +1,48 @@
 #include "esphome.h"
 
-#define CHECK_BIT(var, pos) (((var) >> (pos)) & 1)
-
-class LD2410 : public PollingComponent, public UARTDevice
+class LD2410 : public Component, public UARTDevice
 {
 public:
   LD2410(UARTComponent *parent) : UARTDevice(parent) {}
+  std::vector<uint8_t> bytes;
+  const std::vector<uint8_t> config_header = {0xFD, 0xFC, 0xFB, 0xFA, 0x1C, 0x00};
+  const std::vector<uint8_t> target_header = {0xF4, 0xF3, 0xF2, 0xF1, 0x0D, 0x00};
+  const std::vector<uint8_t> ld2410_end_conf = {0x04, 0x03, 0x02, 0x01};
 
-  BinarySensor *hasTarget = new BinarySensor();
-  BinarySensor *hasMovingTarget = new BinarySensor();
-  BinarySensor *hasStillTarget = new BinarySensor();
-  BinarySensor *lastCommandSuccess = new BinarySensor();
-  Sensor *movingTargetDistance = new Sensor();
-  Sensor *movingTargetEnergy = new Sensor();
-  Sensor *stillTargetDistance = new Sensor();
-  Sensor *stillTargetEnergy = new Sensor();
-  Sensor *detectDistance = new Sensor();
-
-  Number *maxMovingDistanceRange;
-  Number *maxStillDistanceRange;
-  int movingSensitivities[9] = {0};
-  int stillSensitivities[9] = {0};
-  Number *noneDuration;
-
-  long lastPeriodicMillis = millis();
-
-  void setNumbers(Number *maxMovingDistanceRange_, Number *maxStillDistanceRange_, Number *noneDuration_){
-    maxMovingDistanceRange = maxMovingDistanceRange_;
-    maxStillDistanceRange = maxStillDistanceRange_;
-    noneDuration = noneDuration_;
+  void ESP_LOGD_HEX(std::vector<uint8_t> bytes, uint8_t separator) {
+    std::string res;
+    size_t len = bytes.size();
+    char buf[5];
+    for (size_t i = 0; i < len; i++) {
+      if (i > 0) {
+        res += separator;
+      }
+      sprintf(buf, "%02X", bytes[i]);
+      res += buf;
+    }
+    ESP_LOGD("custom", "%s", res.c_str());
   }
 
   void sendCommand(char *commandStr, char *commandValue, int commandValueLen)
   {
-    lastCommandSuccess->publish_state(false);
-    // frame start bytes
-    write_byte(0xFD);
-    write_byte(0xFC);
-    write_byte(0xFB);
-    write_byte(0xFA);
-    // length bytes
-    int len = 2;
-    if (commandValue != nullptr)
+    uint16_t len = 2;
+    if (commandValue != nullptr) {
       len += commandValueLen;
-    write_byte(lowByte(len));
-    write_byte(highByte(len));
-    // command string bytes
-    write_byte(commandStr[0]);
-    write_byte(commandStr[1]);
-    // command value bytes
+    }
+    std::vector<uint8_t> ld2410_conf = {0xFD, 0xFC, 0xFB, 0xFA, lowByte(len), highByte(len), commandStr[0], commandStr[1]};
     if (commandValue != nullptr)
     {
       for (int i = 0; i < commandValueLen; i++)
       {
-        write_byte(commandValue[i]);
+        ld2410_conf.push_back(commandValue[i]);
       }
     }
-    // frame end bytes
-    write_byte(0x04);
-    write_byte(0x03);
-    write_byte(0x02);
-    write_byte(0x01);
-    delay(50);
+    for (int i = 0; i < ld2410_end_conf.size(); i++) 
+    {
+      ld2410_conf.push_back(ld2410_end_conf[i]);
+    }
+    // ESP_LOGD_HEX(ld2410_conf,':');
+    write_array(std::vector<uint8_t>(ld2410_conf.begin(), ld2410_conf.end()));
   }
 
   int twoByteToInt(char firstByte, char secondByte)
@@ -69,154 +50,62 @@ public:
     return (int16_t)(secondByte << 8) + firstByte;
   }
 
-  void handlePeriodicData(char *buffer, int len)
+  void handleTargetData(std::vector<uint8_t> buffer)
   {
-    if (len < 12)
-      return; // 4 frame start bytes + 2 length bytes + 1 data end byte + 1 crc byte + 4 frame end bytes
-    if (buffer[0] != 0xF4 || buffer[1] != 0xF3 || buffer[2] != 0xF2 || buffer[3] != 0xF1)
-      return; // check 4 frame start bytes
-    if (buffer[7] != 0xAA || buffer[len - 6] != 0x55 || buffer[len - 5] != 0x00)
-      return; // data head=0xAA, data end=0x55, crc=0x00
-    /*
-      Data Type: 6th byte
-      0x01: Engineering mode
-      0x02: Normal mode
-    */
-    char dataType = buffer[5];
-    /*
-      Target states: 9th byte
-      0x00 = No target
-      0x01 = Moving targets
-      0x02 = Still targets
-      0x03 = Moving+Still targets
-    */
-    char stateByte = buffer[8];
-    hasTarget->publish_state(stateByte != 0x00);
-    /*
-      Reduce data update rate to prevent home assistant database size glow fast
-    */
-    long currentMillis = millis();
-    if (currentMillis - lastPeriodicMillis < 1000)
-      return;
-    lastPeriodicMillis = currentMillis;
+    TARGETUnion targetUnion;
+    std::copy(buffer.begin(), buffer.end(), targetUnion.bytes);
+    if (id(show_stats).state == 1 && targetUnion.target.type == 0x02 && targetUnion.target.state != 0x00)
+    {
+      int movdist = twoByteToInt(targetUnion.target.movdist, targetUnion.target.movdist2);
+      if (id(movingTargetDistance).state != movdist)
+      {
+        id(movingTargetDistance).publish_state(movdist);
+      }
+      if (id(movingTargetEnergy).state != targetUnion.target.movval)
+      {
+        id(movingTargetEnergy).publish_state(targetUnion.target.movval);
+      }
+      int stadist = twoByteToInt(targetUnion.target.stadist, targetUnion.target.stadist2);
+      if (id(stillTargetDistance).state != stadist)
+      {
+        id(stillTargetDistance).publish_state(stadist);
+      }
+      if (id(stillTargetEnergy).state != targetUnion.target.staval)
+      {
+        id(stillTargetEnergy).publish_state(targetUnion.target.staval);
+      }
+      int decdist = twoByteToInt(targetUnion.target.decdist, targetUnion.target.decdist2);
+      if (id(detectDistance).state != decdist)
+      {
+        id(detectDistance).publish_state(decdist);
+      }
+    }
+    else 
+    {
+      return; 
+    }
+    // Engineering data - datasheet is horrible
+    // if (targetUnion.target.type == 0x01)
+    // }
+  }
 
-    hasMovingTarget->publish_state(CHECK_BIT(stateByte, 0));
-    hasStillTarget->publish_state(CHECK_BIT(stateByte, 1));
-
-    /*
-      Moving target distance: 10~11th bytes
-      Moving target energy: 12th byte
-      Still target distance: 13~14th bytes
-      Still target energy: 15th byte
-      Detect distance: 16~17th bytes
-    */
-    int newMovingTargetDistance = twoByteToInt(buffer[9], buffer[10]);
-    if (movingTargetDistance->get_state() != newMovingTargetDistance)
-      movingTargetDistance->publish_state(newMovingTargetDistance);
-    int newMovingTargetEnergy = buffer[11];
-    if (movingTargetEnergy->get_state() != newMovingTargetEnergy)
-      movingTargetEnergy->publish_state(newMovingTargetEnergy);
-    int newStillTargetDistance = twoByteToInt(buffer[12], buffer[13]);
-    if (stillTargetDistance->get_state() != newStillTargetDistance)
-      stillTargetDistance->publish_state(newStillTargetDistance);
-    int newStillTargetEnergy = buffer[14];
-    if (stillTargetEnergy->get_state() != newStillTargetEnergy)
-      stillTargetEnergy->publish_state(buffer[14]);
-    int newDetectDistance = twoByteToInt(buffer[15], buffer[16]);
-    if (detectDistance->get_state() != newDetectDistance)
-      detectDistance->publish_state(newDetectDistance);
-    if (dataType == 0x01)
-    { // engineering mode
-      // todo: support engineering mode data
+  void handleConfData(std::vector<uint8_t> buffer)
+  {
+    CONFUnion confUnion;
+    std::copy(buffer.begin(), buffer.end(), confUnion.bytes);
+    if (confUnion.conf.cmd == 0x61 && confUnion.conf.cmd_val == 0x01 && confUnion.conf.ack_stat == 0x00 && confUnion.conf.head == 0xAA)
+    {
+      id(maxconfigDistance).publish_state(float(confUnion.conf.max_sta_dist * 0.75));
+      id(allSensitivity).publish_state(confUnion.conf.mov0sen);
+      id(noneDuration).publish_state(confUnion.conf.none);
     }
   }
 
-  void handleACKData(char *buffer, int len)
+  void setConfigMode(bool confenable)
   {
-    if (len < 10)
-      return;
-    if (buffer[0] != 0xFD || buffer[1] != 0xFC || buffer[2] != 0xFB || buffer[3] != 0xFA)
-      return; // check 4 frame start bytes
-    if (buffer[7] != 0x01)
-      return;
-    if (twoByteToInt(buffer[8], buffer[9]) != 0x00)
-    {
-      lastCommandSuccess->publish_state(false);
-      return;
-    }
-    lastCommandSuccess->publish_state(true);
-    switch (buffer[6])
-    {
-    case 0x61: // Query parameters response
-    {
-      if (buffer[10] != 0xAA)
-        return; // value head=0xAA
-      /*
-        Moving distance range: 13th byte
-        Still distance range: 14th byte
-      */
-      maxMovingDistanceRange->publish_state(buffer[12]);
-      maxStillDistanceRange->publish_state(buffer[13]);
-      /*
-        Moving Sensitivities: 15~23th bytes
-        Still Sensitivities: 24~32th bytes
-      */
-      for (int i = 0; i < 9; i++)
-      {
-        movingSensitivities[i] = buffer[14 + i];
-      }
-      for (int i = 0; i < 9; i++)
-      {
-        stillSensitivities[i] = buffer[23 + i];
-      }
-      /*
-        None Duration: 33~34th bytes
-      */
-      noneDuration->publish_state(twoByteToInt(buffer[32], buffer[33]));
-    }
-    break;
-    default:
-      break;
-    }
-  }
-
-  void readline(int readch, char *buffer, int len)
-  {
-    static int pos = 0;
-
-    if (readch >= 0)
-    {
-      if (pos < len - 1)
-      {
-        buffer[pos++] = readch;
-        buffer[pos] = 0;
-      }
-      else
-      {
-        pos = 0;
-      }
-      if (pos >= 4)
-      {
-        if (buffer[pos - 4] == 0xF8 && buffer[pos - 3] == 0xF7 && buffer[pos - 2] == 0xF6 && buffer[pos - 1] == 0xF5)
-        {
-          handlePeriodicData(buffer, pos);
-          pos = 0; // Reset position index ready for next time
-        }
-        else if (buffer[pos - 4] == 0x04 && buffer[pos - 3] == 0x03 && buffer[pos - 2] == 0x02 && buffer[pos - 1] == 0x01)
-        {
-          handleACKData(buffer, pos);
-          pos = 0; // Reset position index ready for next time
-        }
-      }
-    }
-    return;
-  }
-
-  void setConfigMode(bool enable)
-  {
-    char cmd[2] = {enable ? 0xFF : 0xFE, 0x00};
+    char cmd[2] = {confenable ? 0xFF : 0xFE,0x00};
     char value[2] = {0x01, 0x00};
-    sendCommand(cmd, enable ? value : nullptr, 2);
+    sendCommand(cmd, confenable ? value : nullptr, 2);
   }
 
   void queryParameters()
@@ -226,23 +115,45 @@ public:
   }
 
   void setup() override
-  {
-    set_update_interval(15000);
-  }
+  {  }
 
   void loop() override
   {
-    const int max_line_length = 80;
-    static char buffer[max_line_length];
     while (available())
     {
-      readline(read(), buffer, max_line_length);
+      bytes.push_back(read());
+      if (bytes.size() < 6)
+      {
+        continue;
+      }
+      if (doesHeaderMatch(bytes, config_header))
+      {
+        if (bytes.size() < sizeof(CONF))
+        {
+          continue;
+        }
+        handleConfData(bytes);
+        bytes.clear();
+      }
+      else if (doesHeaderMatch(bytes, target_header)) {
+        if (bytes.size() < sizeof(TARGET))
+        {
+          continue;
+        }
+        handleTargetData(bytes);
+        bytes.clear();
+      }
+      else
+      {
+        bytes.erase(bytes.begin());
+        continue;
+      }
     }
   }
 
-  void setEngineeringMode(bool enable)
+  void setEngineeringMode(bool engenable)
   {
-    char cmd[2] = {enable ? 0x62 : 0x63, 0x00};
+    char cmd[2] = {engenable ? 0x62 : 0x63,0x00};
     sendCommand(cmd, nullptr, 0);
   }
 
@@ -250,8 +161,16 @@ public:
   {
     char cmd[2] = {0x60, 0x00};
     char value[18] = {0x00, 0x00, lowByte(maxMovingDistanceRange), highByte(maxMovingDistanceRange), 0x00, 0x00, 0x01, 0x00, lowByte(maxStillDistanceRange), highByte(maxStillDistanceRange), 0x00, 0x00, 0x02, 0x00, lowByte(noneDuration), highByte(noneDuration), 0x00, 0x00};
-    sendCommand(cmd, value, 18);
-    queryParameters();
+    sendCommand(cmd, value, sizeof(value));
+  }
+
+  void setAllSensitivity(int senval)
+  {
+    //  64 00  00 00  FF FF 00 00 01 00  28 00 00 00 02 00 28 00 00 00 04 03 02 01
+    // {cmd  }{dword}{   dgate  }{mword} {   mval   }{sword}{   sval   }{    MFR  }
+    char cmd[2] = {0x64, 0x00};
+    char value[18] = {0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x01, 0x00, lowByte(senval), highByte(senval), 0x00, 0x00, 0x02, 0x00, lowByte(senval), highByte(senval), 0x00, 0x00};
+    sendCommand(cmd, value, sizeof(value));
   }
 
   void factoryReset()
@@ -271,10 +190,97 @@ public:
   {
     char cmd[2] = {0xA1, 0x00};
     char value[2] = {index, 0x00};
-    sendCommand(cmd, value, 2);
+    sendCommand(cmd, value, sizeof(value));
   }
 
-  void update()
+  bool doesHeaderMatch(std::vector<uint8_t> bytes, std::vector<uint8_t> header)
   {
+    bool is_equal = std::equal(header.begin(), header.end(), bytes.begin());
+    return (is_equal == true ? true : false);
   }
+
+/*
+TARGET EXAMPLE DATA
+{F4:F3:F2:F1}:{0D:00}:{02}:{AA}: 02  : 4B:00:  4F  : 00:00 : 64   :  29:00 :{55}: {00} :{F8:F7:F6:F5}
+{  header   }  {len}  {typ}{hd}{state}{mdist}{mval}{stadis}{staval}{decdis} {tl} {chck} {    MFR    }
+*/
+  typedef struct
+  {
+    uint32_t MFR;
+    uint16_t len;
+    uint8_t type;                 // target or engineering
+    uint8_t head;                // fixed head
+    uint8_t state;              // state
+    uint8_t movdist;          // movement distance
+    uint8_t movdist2;          // movement distance
+    uint8_t movval;           // movement energy value
+    uint8_t stadist;        // stationary distance
+    uint8_t stadist2;        // stationary distance
+    uint8_t staval;         // stationary energy value
+    uint8_t decdist;      // detection distance
+    uint8_t decdist2;      // detection distance
+    uint8_t tail;         // tail
+    uint8_t chk;         // unused
+    uint32_t MFR_end ;  // end
+  } TARGET;
+
+  typedef union
+  {
+    TARGET target;
+    uint8_t bytes[sizeof(TARGET)];
+  } TARGETUnion;
+
+/*
+CONF EXAMPLE DATA
+FD:FC:FB:FA MFR[0-3]
+1C:00 len[4-5]
+61:01 CMD[6-7]
+00:00 ACKstat[8-9]
+AA Head [10]
+08 maxDist [11]
+06 maxMovDist[12]
+06 maxStaDist[13]
+1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E:1E (9mov & 9sta sensitivities)
+5A:00 none[32-33]
+04:03:02:01[34-37]
+*/
+  typedef struct
+  {
+    uint32_t MFR;
+    uint16_t len;
+    uint8_t cmd;
+    uint8_t cmd_val;
+    uint8_t ack_stat;
+    uint8_t ack_stathigh;
+    uint8_t head;
+    uint8_t max_dist;
+    uint8_t max_mov_dist;
+    uint8_t max_sta_dist;
+    uint8_t mov0sen;
+    uint8_t mov1sen;
+    uint8_t mov2sen;
+    uint8_t mov3sen;
+    uint8_t mov4sen;
+    uint8_t mov5sen;
+    uint8_t mov6sen;
+    uint8_t mov7sen;
+    uint8_t mov8sen;
+    uint8_t sta0sen;
+    uint8_t sta1sen;
+    uint8_t sta2sen;
+    uint8_t sta3sen;
+    uint8_t sta4sen;
+    uint8_t sta5sen;
+    uint8_t sta6sen;
+    uint8_t sta7sen;
+    uint8_t sta8sen;
+    uint16_t none;
+    uint32_t MFR_end;
+  } CONF;
+
+  typedef union
+  {
+    CONF conf;
+    uint8_t bytes[sizeof(CONF)];
+  } CONFUnion;
 };
